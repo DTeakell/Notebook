8154093b7306dd2387da7b51c577baae557aab0c
Dillon Teakell <teakelld@icloud.com>
2024-11-05 11:06:10 -0500

    Initial Commit

---
+++ Notebook/NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookApp.swift
+//  Notebook
+//
+//  Created by Dillon Teakell on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
